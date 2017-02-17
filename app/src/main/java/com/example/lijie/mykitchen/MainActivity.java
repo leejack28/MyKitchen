5a6793ba2d0d732ffa42c5983c1a5820131dd417
leejack28 <upfighting@sina.com>
2017-02-17 11:11:56 +0800

    添加各种开源库的依赖赖

--- app/src/main/java/com/example/lijie/mykitchen/MainActivity.java
+++ app/src/main/java/com/example/lijie/mykitchen/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.lijie.mykitchen;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import me.majiajie.pagerbottomtabstrip.PagerBottomTabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
+    private PagerBottomTabLayout pbTabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pbTabLayout = (PagerBottomTabLayout) findViewById(R.id.pbTabLayout);
+        pbTabLayout.builder()
+                .addTabItem(R.drawable.kitchen,"厨房")
+                .addTabItem(R.drawable.takeaway,"外卖")
+                .addTabItem(R.drawable.mine,"我的")
+                .setDefaultColor(Color.rgb(0xB2,0xD7,0xD2))
+                .build().setBackgroundColor(Color.rgb(0,79,0x6B));
+
+
     }
 }
